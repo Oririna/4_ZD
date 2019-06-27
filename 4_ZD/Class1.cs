09af60c6e40360a990f4a0a3a0819869a24ff638
Oririna <48158075+Oririna@users.noreply.github.com>
2019-06-27 14:28:57 +0500

    Add files via upload

---
+++ 4_ZD/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ZD
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            List<Point> points = new List<Point> {
+            new Point(0, -10),
+            new Point(6, -9),
+            new Point(10, -3),
+            new Point(10, 3),
+            new Point(6, 9),
+            new Point(0, 10),
+            new Point(-6, 9),
+            new Point(-10, 3),
+            new Point(-10, -3),
+            new Point(-6, -9),
+        };
+            double perimeter = 0;
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                perimeter += points[i].RangeeTo(points[i + 1]);
+            }
+            perimeter += points[points.Count - 1].RangeeTo(points[0]);
+            Console.Write("Периметр = ");
+            perimeter = Math.Round(perimeter, 0);
+            Console.WriteLine(perimeter);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 4_ZD/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_ZD
+{
+    public struct Point : IEquatable<Point>
+    {
+        public Point(double x, double y)
+            : this()
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public bool Equals(Point p1)
+        {
+            return
+                this.X == p1.X &&
+                this.Y == p1.Y;
+        }
+
+        public double RangeeTo(Point p1)
+        {
+            double d = (this.X - p1.X) * (this.X - p1.X)  +
+                (this.Y - p1.Y) * (this.Y - p1.Y);
+            return Math.Sqrt(d);
+        }
+    }
+}
